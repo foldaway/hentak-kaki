b63eb10c0ca75f5b2d3cbf15564041a9c2d44606
Duncan Leo <duncanleo97@gmail.com>
2018-03-17 15:44:35 +0800

    add ten-pack command

---
+++ commands/tenpack.js
@@ -0,0 +1,16 @@
+module.exports = (ctx) => ctx.replyWithMarkdown(`
+*TEN PACK ITEMS*
+1. No. 4
+2. Towel
+3. Admin, Underwear, Socks
+4. Toiletries
+5. Accessories
+6. Sandals
+7. Mess Tin
+8. Sandbag & Range card
+9. RCK
+10. Water bag
+
+Left: ET Blade & Stick, Ammo Pouch, Arc of Fire sticks
+Right: Field pack, ILBV, Helmet, Water canteen
+`);
