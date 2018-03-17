47119569c76cf31bc6d2c46408ccf49e4b867259
Duncan Leo <duncanleo97@gmail.com>
2018-03-17 15:24:59 +0800

    add basic Telegram bot

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log('Hi!');
+require('dotenv').config();
+
+const Telegraf = require('telegraf')
+
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
+bot.command('help', (ctx) => ctx.reply('Try send a sticker!'));
+bot.startPolling();
