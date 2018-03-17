c851a6c311b10b449bcf6e28b64ff65ee027c002
Duncan Leo <duncanleo97@gmail.com>
2018-03-17 15:46:57 +0800

    remove order

--- commands/tenpack.js
+++ commands/tenpack.js
@@ -1,16 +1,22 @@
 module.exports = (ctx) => ctx.replyWithMarkdown(`
 *TEN PACK ITEMS*
-1. No. 4
-2. Towel
-3. Admin, Underwear, Socks
-4. Toiletries
-5. Accessories
-6. Sandals
-7. Mess Tin
-8. Sandbag & Range card
-9. RCK
-10. Water bag
+- No. 4
+- Towel
+- Admin, Underwear, Socks
+- Toiletries
+- Accessories
+- Sandals
+- Mess Tin
+- Sandbag & Range card
+- RCK
+- Water bag
 
-Left: ET Blade & Stick, Ammo Pouch, Arc of Fire sticks
-Right: Field pack, ILBV, Helmet, Water canteen
+_Additional:_
+- ET Blade & Stick
+- Ammo Pouch
+- Arc of Fire sticks
+- Field pack
+- ILBV
+- Helmet
+- Water canteen
 `);
