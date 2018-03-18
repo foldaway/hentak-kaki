29ec6571274c8cdf64626be0eab5960cf2bcd92c
Duncan Leo <duncanleo97@gmail.com>
2018-03-18 14:51:01 +0800

    add more feeds

--- commands/vocation.js
+++ commands/vocation.js
@@ -1,7 +1,16 @@
 const fetch = require('node-fetch');
+const pry = require('pryjs');
+
+const feeds = [
+  'https://www.reddit.com/r/singapore/comments/84rjsk/ns_posting_0318.json', // 01/18
+  'https://www.reddit.com/r/singapore/comments/7jw0fh/ns_postings_0517.json', // 04/17
+  'https://www.reddit.com/r/singapore/comments/7039qd/ns_postings_0317.json', // 03/17
+  'https://www.reddit.com/r/singapore/comments/6hfbvk/ns_postings_0217.json' // 02/17
+];
 
 const hasBodyText = comment => 'body' in comment.data;
-const hasReplies = comment => comment.data.replies.data.children.length > 0 &&
+const hasReplies = comment => comment.data.replies &&
+  comment.data.replies.data.children.length > 0 &&
   comment.data.replies.data.children[0].kind !== 'more'; // More comments. Don't bother to expand.
 const arraySample = array => array[Math.floor(Math.random() * array.length)];
 
@@ -11,9 +20,12 @@ module.exports = async (ctx) => {
     .indexOf(argument.toLowerCase()) > -1;
 
   ctx.replyWithChatAction('typing');
-  const feed = await fetch('https://www.reddit.com/r/singapore/comments/84rjsk/ns_posting_0318.json')
-    .then(r => r.json());
-  const relevantComments = feed[1].data.children
+  const responses = await Promise.all(feeds.map(url => fetch(url)
+    .then(r => r.json())
+    .then(data => data[1].data.children)));
+  const feedComments = responses.reduce((a, b) => a.concat(b));
+  // eval(pry.it)
+  const relevantComments = feedComments
     .filter(hasBodyText)
     .filter(hasArgument)
     .filter(hasReplies);
