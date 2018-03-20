121535c0ebb497ea96505289b9b5318e228edd98
Duncan Leo <duncanleo97@gmail.com>
2018-03-20 23:47:02 +0800

    accept body_html comments

--- commands/ghoststory.js
+++ commands/ghoststory.js
@@ -1,5 +1,10 @@
 const fetch = require('node-fetch');
 
+const TurndownService = require('turndown');
+const turndownService = new TurndownService({
+  strongDelimiter: '*'
+});
+
 const urls = [
   'https://www.reddit.com/r/singapore/comments/5iajz7/share_your_scary_ns_storiesexperiences.json',
   'https://www.reddit.com/r/singapore/comments/4ovtw1/come_share_your_ghostsupernatural_stories.json',
@@ -14,10 +19,10 @@ module.exports = async (ctx) => {
     .then(r => r.json())
     .then(data => data[1].data.children)));
   const comments = responses.reduce((a, b) => a.concat(b))
-    .filter(comment => 'body' in comment.data);
+    .filter(comment => 'body' in comment.data || 'body_html' in comment.data);
   const comment = arraySample(comments);
   ctx.replyWithMarkdown(`
-  *${comment.data.author}:*
-  ${comment.data.body}
+*${comment.data.author}:*
+${turndownService.turndown(comment.data.body || comment.data.body_html)}
   `, { reply_to_message_id: ctx.update.message.message_id });
 };
