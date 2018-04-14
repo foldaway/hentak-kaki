7221e84aa39e99c27a4269d1f53f5134d2304230
Duncan Leo <duncanleo97@gmail.com>
2018-04-14 15:56:56 +0800

    retrieve all posts for random selection

--- commands/meme.js
+++ commands/meme.js
@@ -1,16 +1,30 @@
 const fetch = require('node-fetch');
-const pry = require('pryjs');
 
 const arraySample = array => array[Math.floor(Math.random() * array.length)];
 
 module.exports = async (ctx) => {
-  const feed = await fetch(`https://graph.facebook.com/officialsafmemes/posts?access_token=${process.env.FACEBOOK_APP_ID}|${process.env.FACEBOOK_APP_SECRET}&fields=attachments`)
-    .then(r => r.json());
+  const posts = [];
+  let url = `https://graph.facebook.com/officialsafmemes/posts?access_token=${process.env.FACEBOOK_APP_ID}|${process.env.FACEBOOK_APP_SECRET}&fields=attachments&limit=100`;
+  let response = null;
 
-  const chosenPost = arraySample(feed.data.filter(post => 'attachments' in post)
+  ctx.replyWithChatAction('typing');
+
+  do {
+    response = await fetch(url) // eslint-disable-line no-await-in-loop
+      .then(r => r.json());
+    posts.push(...response.data);
+    url = response.paging.next;
+  } while ('next' in response);
+
+  const chosenPost = arraySample(posts.filter(post => 'attachments' in post)
     .filter(post => post.attachments.data.length > 0));
-  const chosenImage = chosenPost.attachments.data[0];
-  ctx.replyWithPhoto(chosenImage.media.image.src, {
-    caption: chosenImage.description
-  });
+
+  const chosenAtt = chosenPost.attachments.data[0];
+  if (chosenAtt.type === 'photo') {
+    ctx.replyWithPhoto(chosenAtt.media.image.src, {
+      caption: chosenAtt.description
+    });
+  } else {
+    ctx.reply(chosenAtt.url);
+  }
 };
