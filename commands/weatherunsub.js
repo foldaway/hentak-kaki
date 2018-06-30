55a3268ab86f13fb293b85190922a28d60a41051
Duncan Leo <duncanleo97@gmail.com>
2018-06-30 16:33:15 +0800

    add listing of subscriptions

--- commands/weatherunsub.js
+++ commands/weatherunsub.js
@@ -17,6 +17,7 @@ module.exports = {
     ctx.reply('Select a sector to unsubscribe from.', {
       reply_markup: {
         keyboard: subscribedSectors
+          .reverse()
           .map((area) => [{
             text: area
           }]),
@@ -27,7 +28,16 @@ module.exports = {
     });
   },
   responseHandler: async (ctx) => {
+    const subscribedSectors = await lrange(ctx.chat.id, 0, -1);
     const arg = ctx.message.text;
+
+    if (subscribedSectors.indexOf(arg) === -1) {
+      ctx.replyWithMarkdown(
+        'Unknown sector. Action cancelled.',
+        { reply_to_message_id: ctx.update.message.message_id }
+      );
+      return;
+    }
 
     await lrem(ctx.chat.id, 0, arg);
     await lrem(arg, 0, ctx.chat.id);
