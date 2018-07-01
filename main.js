698f8911965d1d434a5f2f0b13b1793947ddc8aa
Duncan Leo <duncanleo97@gmail.com>
2018-07-01 10:46:51 +0800

    add working error handling

--- main.js
+++ main.js
@@ -21,31 +21,44 @@ const stage = new Stage();
 bot.use(session());
 bot.use(stage.middleware());
 
+const errorHandlerFn = (ctx) => (e) => ctx.replyWithMarkdown(
+  `Error occurred:\n\`${e.message}\``,
+  { reply_to_message_id: ctx.update.message.message_id }
+);
+
+const asyncWrapFunc = (func) => async (...args) => {
+  try {
+    await func(...args);
+  } catch (e) {
+    errorHandlerFn(...args)(e);
+  }
+};
+
 // Load all commands
 glob.sync('./commands/**/*.js').forEach((file) => {
   const exp = require(path.resolve(file)); // eslint-disable-line global-require
   const commandName = path.basename(file).replace(/\.js/, '');
   if (exp instanceof Function) {
-    bot.command(commandName, exp);
+    bot.command(commandName, asyncWrapFunc(exp));
   } else if (exp instanceof Object) {
     const scene = new Scene(commandName);
-    scene.enter(exp.initialHandler);
+    scene.enter(asyncWrapFunc(exp.initialHandler));
 
     if ('responseHandlers' in exp) {
       // Fixed-answer handlers
       scene.on('message', (ctx) => {
         if (ctx.update.message.text in exp.responseHandlers) {
-          exp.responseHandlers[ctx.update.message.text](ctx);
+          asyncWrapFunc(exp.responseHandlers[ctx.update.message.text])(ctx);
         } else if ('responseHandler' in exp) {
           // Last resort. Fallback, can be used to show an error message
-          exp.responseHandler(ctx);
+          asyncWrapFunc(exp.responseHandler)(ctx);
         }
         ctx.scene.leave();
       });
     } else {
       // Single catch-all handler, for open-ended responses
       scene.on('message', (ctx) => {
-        exp.responseHandler(ctx);
+        asyncWrapFunc(exp.responseHandler)(ctx);
         ctx.scene.leave();
       });
     }
