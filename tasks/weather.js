3d93fc7a631922c4f3ec416730206af12dd61bc8
Duncan Leo <duncanleo97@gmail.com>
2018-07-03 21:40:56 +0800

    consolidate weather updates

--- tasks/weather.js
+++ tasks/weather.js
@@ -13,6 +13,12 @@ const hset = promisify(redisClient.hset).bind(redisClient);
 const KEY_LATEST_UPDATE_TIMESTAMP = 'WEATHER_LATEST_UPDATE_TIMESTAMP';
 const KEY_LAST_FETCH_UPDATE_FORECASTS = 'WEATHER_LAST_FETCH_UPDATE_FORECASTS';
 
+const CHANGETYPE = {
+  WASCATONE: 'WASCATONE',
+  NOWCATONE: 'NOWCATONE',
+  EXTENDED: 'EXTENDED'
+};
+
 /* eslint-disable camelcase, no-await-in-loop */
 
 module.exports = {
@@ -39,6 +45,9 @@ module.exports = {
     }
     set(KEY_LATEST_UPDATE_TIMESTAMP, latestItem.update_timestamp);
 
+    const areaForecastMap = {};
+    const chatIdsSet = new Set();
+
     for (const area of areas) {
       const latestForecast = latestItem.forecasts
         .find((forecast) => forecast.area === area).forecast;
@@ -50,19 +59,54 @@ module.exports = {
       console.log(`Checking '${area}'. '${previousForecast}' => '${latestForecast}'`);
       hset(KEY_LAST_FETCH_UPDATE_FORECASTS, area, latestForecast);
 
-      let text = '';
+      const areaForecast = {
+        name: area,
+        periodEnd: latestItem.valid_period.end,
+        periodStart: latestItem.valid_period.start,
+        type: null
+      };
 
-      if (latestForecast.match(/Thunder/i) && (previousForecast === latestForecast || redisLastFetchedForecast === latestForecast)) {
-        text = `Cat 1 status extended for *${area}* (until ${dateFormat(latestItem.valid_period.end, 'HHmm')})\n`;
+      if (latestForecast.match(/Thunder/i) && (previousForecast.match(/Thunder/i) || redisLastFetchedForecast === latestForecast)) {
+        areaForecast.type = CHANGETYPE.EXTENDED;
       } else if (latestForecast.match(/Thunder/i)) {
-        text = `Lighting risk is now Cat 1 for *${area}* (until ${dateFormat(latestItem.valid_period.end, 'HHmm')})\n`;
+        areaForecast.type = CHANGETYPE.NOWCATONE;
       } else if (previousForecast.match(/Thunder/i)) {
-        text = `Cat 1 status has been lifted for *${area}*\n`;
+        areaForecast.type = CHANGETYPE.WASCATONE;
+      }
+
+      if (areaForecast.type) {
+        areaForecastMap[area] = areaForecast;
+
+        const chatIds = await lrange(area, 0, -1);
+        chatIds.forEach(chatIdsSet.add.bind(chatIdsSet));
+      }
+    }
+
+    const formatFunc = (af) => `- ${af.name}`;
+
+    for (const chatId of chatIdsSet) {
+      const chatIdAreas = (await lrange(chatId, 0, -1))
+        .filter((area) => area in areaForecastMap)
+        .map((area) => areaForecastMap[area]);
+      const nowAFs = chatIdAreas.filter((area) => area.type === CHANGETYPE.NOWCATONE);
+      const wasAFs = chatIdAreas.filter((area) => area.type === CHANGETYPE.WASCATONE);
+      const extAFs = chatIdAreas.filter((area) => area.type === CHANGETYPE.EXTENDED);
+
+      let text = '';
+      if (nowAFs.length > 0) {
+        text += `It's now Cat 1 (until ${dateFormat(nowAFs[0].periodEnd, 'HHmm')})\n*${nowAFs.map(formatFunc).join('\n')}*\n`;
+      }
+
+      if (wasAFs.length > 0) {
+        text += `Cat 1 downgraded (since ${dateFormat(wasAFs[0].periodStart, 'HHmm')})\n*${wasAFs.map(formatFunc).join('\n')}*\n`;
+      }
+
+      if (extAFs.length > 0) {
+        text += `Cat 1 extended (until ${dateFormat(extAFs[0].periodEnd, 'HHmm')})\n*${extAFs.map(formatFunc).join('\n')}*\n`;
       }
 
       if (text.length > 0) {
-        const chatIds = await lrange(area, 0, -1);
-        chatIds.forEach((chatId) => bot.sendMessage(chatId, text, { parse_mode: 'markdown' }));
+        bot.sendMessage(chatId, text, { parse_mode: 'markdown' });
       }
     }
   }
