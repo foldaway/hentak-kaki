c403adacdc17d35fe9271ba95d4f811f7c769a46
Duncan Leo <duncanleo97@gmail.com>
2018-08-12 12:49:10 +0800

    initial Sequelize-generated files

---
+++ config/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  development: {
+    username: process.env.DATABASE_USER || process.env.USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: 'hentak_development',
+    host: '127.0.0.1',
+    dialect: 'postgres'
+  },
+  test: {
+    username: root,
+    password: null,
+    database: 'hentak_test',
+    host: '127.0.0.1',
+    dialect: 'postgres'
+  },
+  production: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres'
+  }
+};
