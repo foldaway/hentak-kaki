8d94b6930e511afa4cd4e080da51f8d62ec91248
Duncan Leo <duncanleo97@gmail.com>
2018-08-22 14:54:30 +0800

    add scheduler script
    runs tasks immediately

---
+++ scheduler.js
@@ -0,0 +1,13 @@
+const glob = require('glob');
+const path = require('path');
+
+const Telegram = require('telegraf/telegram');
+
+const telegram = new Telegram(process.env.TELEGRAM_BOT_TOKEN);
+
+glob.sync('./tasks/**/*.js').forEach((file) => {
+  const exp = require(path.resolve(file)); // eslint-disable-line global-require
+  const task = path.basename(file).replace(/\.js/, '');
+  console.log(`Running '${task}' now.`);
+  exp.func(telegram);
+});
