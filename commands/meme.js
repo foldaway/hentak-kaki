db0d2e0bf06c43526062369a1b8c767ee9d52146
Duncan Leo <hello@duncanleo.me>
2019-03-30 22:24:35 +0800

    switch to puppeteer

--- commands/meme.js
+++ commands/meme.js
@@ -1,31 +1,36 @@
-const webdriver = require('selenium-webdriver');
-const { By, until, Browser } = require('selenium-webdriver');
-const chrome = require('selenium-webdriver/chrome');
+const puppeteer = require('puppeteer');
 
 /* eslint-disable no-await-in-loop */
 
-let options = new chrome.Options()
-  .addArguments('--headless');
+const options = {
+  headless: true
+};
 
 if (process.env.GOOGLE_CHROME_SHIM) {
-  options = options.addArguments('--no-sandbox')
-    .addArguments('--disable-gpu')
-    .setChromeBinaryPath(process.env.GOOGLE_CHROME_SHIM);
+  options.args = ['--no-sandbox', '--disable-gpu'];
+  options.executablePath = process.env.GOOGLE_CHROME_SHIM;
 }
 
 module.exports = async (ctx) => {
   await ctx.reply('Fetching, this will take a while...');
 
-  let driver = new webdriver.Builder()
-    .forBrowser(Browser.CHROME)
-    .setChromeOptions(options)
-    .build();
-  await driver.navigate().to('https://www.instagram.com/memedefsg/');
-  await driver.wait(until.elementLocated(By.css('article')), 10000);
-  const posts = await driver.findElements(By.css('a[href^="/p/"]'));
+  const browser = await puppeteer.launch(options);
+  const page = await browser.newPage();
+
+  await page.goto('https://www.instagram.com/memedefsg/');
+  await page.waitForSelector('article', { timeout: 10000 });
+
+  // Load more posts
+  for (let i = 0; i < 5; i += 1) {
+    await page.evaluate(() => window.scrollBy(0, window.innerHeight));
+    await page.waitFor(600);
+  }
+
+  const posts = await page.$$('a[href^="/p/"]');
+  console.log(`Loaded ${posts.length} posts`);
 
   const chosenPost = posts[Math.floor(Math.random() * (posts.length - 1))];
-  await driver.executeScript('arguments[0].scrollIntoView()', chosenPost);
+  await chosenPost.focus();
 
   ctx.replyWithChatAction('typing');
 
@@ -35,19 +40,30 @@ module.exports = async (ctx) => {
     console.error(e);
   }
 
-  await driver.wait(until.elementLocated(By.css('div[role="dialog"]')), 1500);
+  await page.waitForSelector('div[role="dialog"]', { timeout: 1500 });
 
-  let img = await chosenPost.findElement(By.css('img')).getAttribute('src');
+  let img = null;
   let caption = '(no caption)';
 
   try {
-    await driver.wait(until.elementLocated(By.css('a[title="memedefsg"]')), 2000);
-    const authorElem = (await driver.findElements(By.css('a[title="memedefsg"]')))[1];
-    caption = await authorElem.findElement(By.xpath('..')).findElement(By.xpath('..')).getText();
-    caption = caption.replace(/\s?memedefsg\s?/i, '');
+    await page.waitForSelector('a[title="memedefsg"]', { timeout: 2000 });
+    const authorElem = (await page.$$('a[title="memedefsg"]'))[1];
+    if (authorElem) {
+      const authorElemGrandParent = (await authorElem.$x('../..'))[0];
+      caption = await (await authorElemGrandParent.getProperty('textContent')).jsonValue();
+      caption = caption.replace(/\s?memedefsg\s?/i, '');
+    }
   } catch (e) { console.error(e); }
 
-  await driver.quit();
+  try {
+    await page.waitForFunction(() => document.querySelectorAll('article').length >= 2, { timeout: 3000 });
+    const article = (await page.$$('article'))[1];
+    const imgElem = await article.$('img[style*="object-fit"]');
+    img = await (await imgElem.getProperty('src')).jsonValue();
+  } catch (e) { console.error(e); }
+
+  await browser.close();
+
   if (img) {
     ctx.replyWithPhoto(img, {
       caption
